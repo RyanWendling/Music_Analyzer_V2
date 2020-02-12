6778f9b6fade79df7eef758b6c253393d7ac14ec
RyanWendling <wendlingryan@gmail.com>
2020-02-11 19:11:46 -0800

    non-class version, doesn't work

--- imgscraper.js
+++ imgscraper.js
@@ -2,81 +2,63 @@ const Scraper = require("images-scraper");
 const fs = require("fs");
 const request = require("request");
 
-/**
- * @param {string} currentArtist artist name we will search for
- */
+// TODO: check hashmap / filesystem to see if image has already been saved for currentArtist
 
-class ImageScrapeAndSave {
-  constructor(currentArtist) {
-    this.currentArtist = currentArtist;
-  }
+function download(uri, filename, callback) {
+  console.log(filename);
+  request.head(uri, function(err, res, body) {
+    console.log("content-type:", res.headers["content-type"]);
+    console.log("content-length:", res.headers["content-length"]);
 
-  // TODO: check hashmap / filesystem to see if image has already been saved for currentArtist
-
-  static download(uri, filename, callback) {
-    console.log(filename);
-    request.head(uri, function(err, res, body) {
-      console.log("content-type:", res.headers["content-type"]);
-      console.log("content-length:", res.headers["content-length"]);
-
-      request(uri)
-        .pipe(fs.createWriteStream(`${__dirname}/SavedImages/${filename}`))
-        .on("close", callback);
-    });
-  }
-
-  static async ScrapeAndSave(theCurrentArtist) {
-    // Below used to be seperate setup
-    const google = new Scraper.Google({
-      keyword: theCurrentArtist,
-      limit: 2,
-      puppeteer: {
-        headless: true
-      },
-      tbs: {
-        // every possible tbs search option, some examples and more info: http://jwebnet.net/advancedgooglesearch.html
-        isz: undefined, // options: l(arge), m(edium), i(cons), etc.
-        itp: undefined, // options: clipart, face, lineart, news, photo
-        ic: undefined, // options: color, gray, trans
-        sur: undefined // options: fmc (commercial reuse with modification), fc (commercial reuse), fm (noncommercial reuse with modification), f (noncommercial reuse)
-      }
-    });
-    (async () => {
-      const results = await google.start();
-      try {
-        let EndOfFileNumm = 0;
-        // Create folder for images, if folder doesn't yet exist
-        if (!fs.existsSync(`${__dirname}/SavedImages/`)) {
-          fs.mkdirSync(
-            `${__dirname}/SavedImages/`,
-            { recursive: true },
-            err => {
-              if (err) throw err;
-            }
-          );
-        }
-        results.forEach(item => {
-          EndOfFileNumm += 1;
-          //let url = item.url;
-          //const name = url.lastIndexOf("/");
-          //const filename = url.substring(name + 1);
-          //console.log(filename);
-          ImageScrapeAndSave.download(
-            item.url,
-            `filename${EndOfFileNumm}.jpg`,
-            function() {
-              console.log("done");
-            }
-          );
-        });
-      } catch (err) {
-        console.log("err", err);
-      }
-      //console.log("results", results);
-    })();
-  }
+    request(uri)
+      .pipe(fs.createWriteStream(`${__dirname}/SavedImages/${filename}`))
+      .on("close", callback);
+  });
 }
 
-ImageScrapeAndSave.ScrapeAndSave("pikachu");
+function ScrapeAndSave(theCurrentArtist) {
+  // Below used to be seperate setup
+  const google = new Scraper.Google({
+    keyword: theCurrentArtist,
+    limit: 2,
+    puppeteer: {
+      headless: true
+    },
+    tbs: {
+      // every possible tbs search option, some examples and more info: http://jwebnet.net/advancedgooglesearch.html
+      isz: undefined, // options: l(arge), m(edium), i(cons), etc.
+      itp: undefined, // options: clipart, face, lineart, news, photo
+      ic: undefined, // options: color, gray, trans
+      sur: undefined // options: fmc (commercial reuse with modification), fc (commercial reuse), fm (noncommercial reuse with modification), f (noncommercial reuse)
+    }
+  });
+  (async () => {
+    const results = await google.start();
+    try {
+      let EndOfFileNumm = 0;
+      // Create folder for images, if folder doesn't yet exist
+      if (!fs.existsSync(`${__dirname}/SavedImages/`)) {
+        fs.mkdirSync(`${__dirname}/SavedImages/`, { recursive: true }, err => {
+          if (err) throw err;
+        });
+      }
+      results.forEach(item => {
+        EndOfFileNumm += 1;
+        //let url = item.url;
+        //const name = url.lastIndexOf("/");
+        //const filename = url.substring(name + 1);
+        //console.log(filename);
+        download(item.url, `filename${EndOfFileNumm}.jpg`, function() {
+          console.log("done");
+        });
+      });
+    } catch (err) {
+      console.log("err", err);
+    }
+    //console.log("results", results);
+  })();
+}
 
-module.exports = ImageScrapeAndSave;
+ScrapeAndSave("pikachu");
+
+//module.exports = ImageScrapeAndSave;
