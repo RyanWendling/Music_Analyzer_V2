05d1d60ebed2dadf0a572c1efa74f9b904c9c9aa
RyanWendling <wendlingryan@gmail.com>
2020-03-17 20:40:09 -0700

    got image downloader working

--- SimilarArtistAnalyzer.js
+++ SimilarArtistAnalyzer.js
@@ -23,14 +23,45 @@ function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+async function downloadFile(url, path) {
+  const res = await fetch(url);
+  const fileStream = fileServer.createWriteStream(path);
+  await new Promise((resolve, reject) => {
+    res.body.pipe(fileStream);
+    res.body.on("error", err => {
+      reject(err);
+    });
+    fileStream.on("finish", function() {
+      resolve();
+    });
+  });
+}
+
+// TODO: ADD FUNCTIONALITY() TO GET IMAGES VIA DEEZER API. WILL PERFORM CHECK BEFOREHAND TO SEE IF IMAGES EXIST IN IMAGE FOLDER.
+async function DownloadAndCheckArtistArtwork(anArtistName) {
+  if (fileServer.existsSync(`./SavedImages/${anArtistName}250`)) {
+    return true;
+  }
+  try {
+    const anAPIResponse = await fetch(`https://api.deezer.com/artist/${anArtistName}`);
+    const anAPIResponseJSON = await anAPIResponse.json();
+    const downloadFileResponse = downloadFile(anAPIResponseJSON.picture_medium, `./SavedImages/${anArtistName}250.jpg`);
+  } catch (e) {
+    console.log(`${e} error downloading image`);
+    return false;
+  }
+  return true;
+}
+
 // Will take API response, parse out artist name and similarity rating, compare to importedArtists map to see if artist already exists (remove whitespace, make lowercase).
 // If new artist doesn't exist within importedArtists map, artistname and similarity rating will then be added to resultsMap. resultsMap will be used after API calls are complete for additional calculations.
-function CalculateAPIResults(apiResponse, importedArtists, resultsMap = new Map(), multiplier) {
+async function CalculateAPIResults(apiResponse, importedArtists, resultsMap = new Map(), multiplier) {
   console.log("in calculateAPIResults");
-  apiResponse.similarartists.artist.forEach(curArtist => {
+  apiResponse.similarartists.artist.forEach(async curArtist => {
     // console.log(curArtist.name);
     if (!importedArtists.has(curArtist.name.toLowerCase())) {
       if (!resultsMap.has(curArtist.name)) {
+        const artworkExists = await DownloadAndCheckArtistArtwork(curArtist.name);
         resultsMap.set(curArtist.name, parseFloat(curArtist.match) * multiplier);
       } else {
         resultsMap.set(curArtist.name, resultsMap.get(curArtist.name) + parseFloat(curArtist.match) * multiplier);
@@ -78,6 +109,8 @@ function readInLibrary() {
 async function AnalyzeMusic() {
   const artistsMap = await readInLibrary();
   const resultsFromCalculateAPIResults = new Map();
+  // ASGFAWGTAERGAEWGE
+  const resultttt = await DownloadAndCheckArtistArtwork("Gorillaz");
   for (let [key, value] of artistsMap) {
     console.log(key + " = " + value);
     const anAPIResponse = await fetch(`http://ws.audioscrobbler.com/2.0/?method=artist.getsimilar&artist=${encodeURI(key)}&autocorrect=1&api_key=f28c377cc9c4485831f3bcf5b9e1670a&format=json`);
@@ -86,7 +119,6 @@ async function AnalyzeMusic() {
       continue;
     }
     //console.log(anAPIResponseJSON);
-
     CalculateAPIResults(anAPIResponseJSON, artistsMap, resultsFromCalculateAPIResults, value);
   }
   // const sortedResults = new Map([...resultsFromCalculateAPIResults.entries()].sort((a, b) => b[1] - a[1]));
