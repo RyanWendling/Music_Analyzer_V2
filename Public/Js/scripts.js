c74fd25803968559dd557f8456459623b1a6df18
RyanWendling <wendlingryan@gmail.com>
2020-05-14 19:35:18 -0700

    - responsive navbar redesign complete.

--- Public/Js/scripts.js
+++ Public/Js/scripts.js
@@ -19,3 +19,10 @@ for (let i = 0; i < listItems.length; i++) {
 
   listItems[i].appendChild(holdingBox);
 } */
+
+let mainNav = document.getElementById("js-menu");
+let navBarToggle = document.getElementById("js-navbar-toggle");
+
+navBarToggle.addEventListener("click", function() {
+  mainNav.classList.toggle("active");
+});
